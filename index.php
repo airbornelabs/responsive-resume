c0ae2083f8bf7ea4a88e1a90358d60f11b2a817b
Anthony Terrell <hello@airbornelabs.net>
2013-10-31 06:07:07 -0500

    GA removal

--- index.php
+++ index.php
@@ -17,17 +17,6 @@
     $(document).foundationAccordion();
   </script>
   <script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-11602733-8']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-  </script>
-  <script type="text/javascript">
     $(function(){
       $('#tweets').tweetable({username: 'anthonyterrell', time: false, limit: 1, replies: true, position: 'append'});
     });
